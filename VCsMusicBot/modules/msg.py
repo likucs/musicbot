846007fa03b5bbc7c8b38a5c28a8b9e5965f3dee
Liku cs <83144124+likucs@users.noreply.github.com>
2021-07-14 17:35:16 +0530

    Update msg.py

--- VCsMusicBot/modules/msg.py
+++ VCsMusicBot/modules/msg.py
@@ -1,7 +1,7 @@
 import os
 from VCsMusicBot.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL
 class Messages():
-      START_MSG = "**Hello 👋 Friend [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels By @iAmLiKu [LiKu Cs].\n\n✅ Hits /help for more info."
+      START_MSG = "**Hello 👋 Friend [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels By @iAmLiKu1 [LiKu Cs].\n\n✅ Hits /help for more info."
       HELP_MSG = [
         ".",
 f"""
